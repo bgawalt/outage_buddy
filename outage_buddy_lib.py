fe8779a61bfda8d089566d5a233ce0b9322b64a3
Brian Gawalt <bgawalt@gmail.com>
2017-09-14 23:32:18 -0700

    Started building the OutageBuddy class

--- outage_buddy_lib.py
+++ outage_buddy_lib.py
@@ -3,6 +3,7 @@ import tweepy
 
 from datetime import datetime
 
+
 def ReadConfig(filename):
     """Read a config file and return a dictionary of its settings.
 
@@ -22,13 +23,13 @@ def ReadConfig(filename):
     or if any line of the config file isn't formatted correctly, it'll raise a
     ValueError.
     """
-    with open(filename, 'r') as infile:
+    with open(filename, "r") as infile:
         split_lines = [line.strip().split(" = ") for line in infile]
     if not all([len(line) == 2 for line in split_lines]):
         raise ValueError("Config has an invalid line")
     config = {pair[0]: pair[1] for pair in split_lines}
-    expected_keys = ['CONSUMER_KEY', 'CONSUMER_SECRET', 'ACCESS_KEY',
-                     'ACCESS_SECRET', 'OWNERS_SPREADSHEET']
+    expected_keys = ["CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_KEY",
+                     "ACCESS_SECRET", "OWNERS_SPREADSHEET"]
     if not all([ex_key in config for ex_key in expected_keys]):
         raise ValueError("Config missing an expected key")
     return config
@@ -46,6 +47,7 @@ def CreateAPI(config):
 
 
 class Account(object):
+    """Details of an account that needs monitoring."""
 
     def __init__(self, name, owner, hours_per_update):
         self._name = name
@@ -69,6 +71,20 @@ class Account(object):
         delta = now - most_recent_time
         delta_hours = float(delta.total_seconds())/3600
         return delta_hours < self._rate
+
+
+class OutageBuddy(object):
+
+    def __init__(self, config):
+        self._api = CreateAPI(config)
+        sheet_url = ("https://spreadsheets.google.com/feeds/download/"
+                     "spreadsheets/Export?key=%s&exportFormat=csv" % (
+                        config["OWNERS_SPREADSHEET"],))
+        with urllib2.urlopen(urllib2.Request(sheet_url)) as infile:
+            self._accounts = [Account.FromCSVLine(line) for line in infile
+                              if line[0] != "#"]
+        self._followers = [] # TODO: Get followers
+
 
 if __name__ == "__main__":
     config = ReadConfig(sys.argv[1])
